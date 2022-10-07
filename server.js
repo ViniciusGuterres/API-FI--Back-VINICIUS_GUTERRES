df14c5f329f503140ffc9cbf47e2eaf0319b4b9c
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2022-10-06 22:03:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const createTablesModel = require('./models/createTables.js').createTables;
 
 require('./routes/index.js')(app);
 
 app.use(cors());
 app.use(bodyParser.json());
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server on in port 3000');
+
+    // Create tables if doesn' exists
+    createTablesModel().then(({ err, data }) => {
+        if (err) {
+            console.log('Tables already created');
+        } else {
+            console.log('Tables User_VG and Movies_VG created successfully');
+        }
+    })
+});
